1ee864e84a1453e507ea0b2f8d3f1305c255eae2
sorawat <s.tanthikun802@mybvc.ca>
2023-12-05 21:19:25 -0700

    feat: init generate monthly report handler

--- internal/handler/reporthdl/report.go
+++ internal/handler/reporthdl/report.go
@@ -1,10 +1,16 @@
 package reporthdl
 
 import (
+	"errors"
 	"net/http"
+	"time"
 
+	apiresponses "github.com/CLCM3102-Ice-Cream-Shop/backend-report-service/internal/handler/apiResponses"
+	"github.com/CLCM3102-Ice-Cream-Shop/backend-report-service/internal/helper/logger"
+	"github.com/CLCM3102-Ice-Cream-Shop/backend-report-service/internal/models"
 	"github.com/CLCM3102-Ice-Cream-Shop/backend-report-service/internal/service"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 )
 
 type HTTPHandler struct {
@@ -17,5 +23,27 @@ func NewHTTPHandler(reportSrv service.Report) HTTPHandler {
 
 func (hdl *HTTPHandler) GenerateMontlyReport(c echo.Context) error {
 
-	return c.JSON(http.StatusOK, nil)
+	dateStr := c.Param("date")
+	if dateStr == "" {
+		err := errors.New("date is empty")
+		logger.Error(err.Error())
+		return echo.NewHTTPError(http.StatusBadRequest, apiresponses.InvalidInputError(err))
+	}
+
+	date, err := time.Parse(time.DateOnly, dateStr)
+	if err != nil {
+		logger.Error("can't parse date to format YYYY-MM-DD", zap.Error(err))
+		return echo.NewHTTPError(http.StatusBadRequest, apiresponses.InvalidInputError(err))
+	}
+
+	err = hdl.reportSrv.GenerateMontlyReport(date)
+	if err != nil {
+		logger.Error("can not generte monthly report", zap.Error(err))
+		return echo.NewHTTPError(http.StatusBadRequest, apiresponses.InternalError(err))
+	}
+
+	var res models.GenerateMonthlyReportResponse
+	res.CommonResponse = apiresponses.SuccessResponse()
+
+	return c.JSON(http.StatusOK, res)
 }
